694252ff331efc06255f170a10fbbd69b4193b15
SamuelBridges <bridges.sam@gmail.com>
2021-03-26 12:13:28 +0000

    Added Tokenising

--- [1]nlp_tokenise.py
+++ [1]nlp_tokenise.py
@@ -7,7 +7,6 @@
 #   nltk.download('wordnet')
 #   nltk.download('averaged_perceptron_tagger')
 
-from nltk.tag import pos_tag
 from nltk.corpus import twitter_samples
 
 positive_tweets = twitter_samples.strings('positive_tweets.json')
@@ -15,4 +14,4 @@ negative_tweets = twitter_samples.strings('negative_tweets.json')
 text = twitter_samples.strings('tweets.20150430-223406.json')
 tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
 
-print(pos_tag(tweet_tokens[0]))
+print(tweet_tokens[0])
